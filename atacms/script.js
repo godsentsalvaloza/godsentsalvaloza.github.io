4f5802b33c8a56c3cde237ca2c9f2d4ea9175e2c
Godsent Salvaloza <85384933+godsentsalvaloza@users.noreply.github.com>
2023-05-31 16:57:50 +0800

    version 0.02 release 9 deployment.
    Many features added

--- atacms/script.js
+++ atacms/script.js
@@ -3,3 +3,24 @@
 
 
 //alert("Under Construction")
+
+//ADDITION GAME
+
+//START BUTTON ON CLICK
+    document.querySelector("#getStarted").onclick = function() {
+        const startButton = document.querySelector("#mainAboutContainer");
+
+        const firstQuestion = document.querySelector ("#firstQuestionContainer");
+
+        startButton.style.display = "none"
+        
+        firstQuestion.style.display = "block"
+
+    }
+//</>START BUTTON ON CLICK
+
+//FIRST QUESTION 
+  
+
+
+//</>ADDITION GAME
