358e642378320ca992d3ecaf1d70cf8a54661fec
Godsent Salvaloza <85384933+godsentsalvaloza@users.noreply.github.com>
2023-06-05 16:55:49 +0800

    Based on version 0.02 release 10

--- atacms/script.js
+++ atacms/script.js
@@ -1,26 +1,48 @@
 //EXPERIMENTAL
 
-
-
-//alert("Under Construction")
-
 //ADDITION GAME
 
+
+//VARIABLES
+const startContainer = document.querySelector ("#startContainer");
+
+const defeatContainer = document.querySelector ("#defeatContainer");
+
+const firstQuestionContainer = document.querySelector ("#firstQuestion");
+const firstQuestionFirstOption = document.querySelector ("#firstQuestionFirstOption");
+const firstQuestionSecondOption = document.querySelector ("#firstQuestionSecondOption");
+const firstQuestionThirdOption = document.querySelector ("#firstQuestionThirdOption");
+const firstQuestionFourthOption = document.querySelector ("#firstQuestionFourthOption");
+//</>VARIABLES
+
+
+
 //START BUTTON ON CLICK
-    document.querySelector("#getStarted").onclick = function() {
-        const startButton = document.querySelector("#mainAboutContainer");
-
-        const firstQuestion = document.querySelector ("#firstQuestionContainer");
-
-        startButton.style.display = "none"
-        
-        firstQuestion.style.display = "block"
-
-    }
+ document.querySelector("#getStarted").onclick = function() {
+    startContainer.style.display = "none"
+    firstQuestionContainer.style.display = "block"
+ }
 //</>START BUTTON ON CLICK
 
+
+
 //FIRST QUESTION 
-  
+document.querySelector("#firstQuestionFirstOption").onclick = function() {
+    firstQuestionContainer.style.display = "none"
+}
+document.querySelector("#firstQuestionSecondOption").onclick = function() {
+    firstQuestionContainer.style.display = "none"
+    defeatContainer.style.display = "block"
+}
+document.querySelector("#firstQuestionThirdOption").onclick = function() {
+    firstQuestionContainer.style.display = "none"
+    defeatContainer.style.display = "block"
+}
+document.querySelector("#firstQuestionFourthOption").onclick = function() {
+    firstQuestionContainer.style.display = "none"
+    defeatContainer.style.display = "block"
+}
+//</>FIRST QUESTION 
 
 
 //</>ADDITION GAME
